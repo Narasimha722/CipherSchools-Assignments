f0d11efc3b31bf6f25b224e17b63308673489ff8
Asam Narasimha Reddy <111991981+Narasimha722@users.noreply.github.com>
2023-07-12 10:46:35 +0530

    Update Lecture7-cipherschools.cpp

--- Lecture7-cipherschools.cpp
+++ Lecture7-cipherschools.cpp
@@ -10,3 +10,30 @@ int main(){
 	
 	return 0;
 }
+
+
+
+ // Infinite loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int password;
+	int count=0;
+//	while(1){
+//		cout<<"choclates ";
+//		count++;
+//		if(count == 100){
+//			break;
+//		}
+//	}
+
+    for(int i=0; ;i++){
+//    	cout<<"pranav"<<" ";
+        cout<<i<<" ";
+        if(i>100) break;
+	}
+	
+	return 0;
+}
